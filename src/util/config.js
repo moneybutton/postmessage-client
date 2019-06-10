95b1622a342c47a191f8498dbd9cc4e2bc0f5ba1
Miguel Duarte <miguelfeliped@gmail.com>
2019-06-10 17:22:28 -0300

    https://github.com/moneybutton/express-paymail/issues/3 (#2827)
    * https://github.com/moneybutton/express-paymail/issues/3
    Fix paymail constants.
    
    * v0.22.0
    
    * Fix config.

--- src/util/config.js
+++ src/util/config.js
@@ -6,18 +6,6 @@ const config = new MoneyButtonConfigBuilder()
     'MONEY_BUTTON_WEBAPP_PROXY_URI',
     process.env.MONEY_BUTTON_WEBAPP_PROXY_URI
   )
-  .addValue(
-    'MONEY_BUTTON_PUBLIC_CLIENT_IDENTIFIER',
-    process.env.MONEY_BUTTON_PUBLIC_CLIENT_IDENTIFIER
-  )
-  .addValue(
-    'MONEY_BUTTON_CONFIDENTIAL_CLIENT_IDENTIFIER',
-    process.env.MONEY_BUTTON_CONFIDENTIAL_CLIENT_IDENTIFIER
-  )
-  .addValue(
-    'MONEY_BUTTON_CONFIDENTIAL_CLIENT_SECRET',
-    process.env.MONEY_BUTTON_CONFIDENTIAL_CLIENT_SECRET
-  )
   .build()
 
 export default config
